8593723d7ab0550ef208324a89936bc013b3e7a0
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2025-01-28 19:51:52 +0100

    style: :art: Add some footers for the chatbot's interface!

---
+++ ollama-bot/footers.py
@@ -0,0 +1,17 @@
+footer = """
+<style>
+.footer {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: #333333;
+    color: #ffffff;
+    padding: 10px;
+    text-align: center;
+}
+</style>
+<div class="footer">
+    <p>Created by Sulaiman Shamasna - <a href="https://www.linkedin.com/in/sulaiman-shamasna-9587b5123" target="_blank">LinkedIn</a> | <a href="https://github.com/sulaiman-shamasna" target="_blank">GitHub</a></p>
+</div>
+"""
